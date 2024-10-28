2e2cbe94ced0ec2b17488b2f7b9e5f21ae5418a2
Horino Yutaro <yutaro.horino@gmail.com>
2024-10-28 16:18:31 +0900

    feat: tailwind を ViewComponent にも適用

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
     '../self-branding/app/views/**/*.html.erb',
+    '../self-branding/app/components/**/*.html.erb',
     '../self-branding/app/helpers/**/*.rb',
     '../self-branding/app/assets/stylesheets/**/*.css',
     '../self-branding/app/javascript/**/*.js'
